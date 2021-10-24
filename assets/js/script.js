3f6d8f194492ac77d0b5878ae6390a511e46bf4b
Jeremy Huynh <jeahuynh18@gmail.com>
2021-10-23 19:59:16 -0700

    added global variables for script.js

---
+++ assets/js/script.js
@@ -0,0 +1,7 @@
+//global variables
+var currentDay = $("#currentDay");
+var currentDayEl = moment().format("MMMM do YYYY, h:mm:ss a");
+var timeEl = $(".time-block");
+var currentHr = moment().format("H")
+var submitBtnEl = $(".saveBtn")
+//display the schedule and save the tasks to local storage
